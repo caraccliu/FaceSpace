86e6057532dceb80eac69cc356caabb29782430c
liucaocao <cliu567@uwo.ca>
2017-02-23 10:24:56 -0500

    change to accountcreation.js file

--- src/main/js/accountcreation.js
+++ src/main/js/accountcreation.js
@@ -1,4 +1,56 @@
 import React from 'react';
+var AccountComponent = React.createClass({
+
+    // Used to initialize state
+    getInitialState () {
+        return {
+            name : "",
+            Message : "",
+        }
+    },
+
+    handleNameChange (e) {
+        // Prevent following the link.
+        e.preventDefault();
+        this.setState({ name : e.target.value , Message : "..."});
+    },
+
+    handleSubmit(e) {
+        // Prevents reinitialization
+        e.preventDefault();
+        let name = this.state.name;
+        fetch('http://localhost:8080/AccountCreation/createAccount?'
+            + 'userName=' + name , {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res =>{
+            if(res.ok){
+            this.setState({Message: 'Account:'+name+' created!'});
+        }
+    else{
+            this.setState({Message: 'Account:'+name+' was already taken!'});
+        }
+    })
+    },
+
+    render () {
+        return (
+            <div>
+            <form onSubmit={this.handleSubmit}>
+    <label>
+        <input type="text" defaultValue={this.state.name} onChange={this.handleNameChange}/>
+    </label>
+        <input type="submit" value="Create Account!" />
+            </form>
+            Name: {this.state.name}
+    <br/>
+        Message: {this.state.Message}
+    </div>
+    );
+    }
+});
 
 export class AccountCreation extends React.Component {
 
@@ -9,7 +61,7 @@ export class AccountCreation extends React.Component {
     render() {
         return(
             <div>
-                Account creation elements go here.
+                <AccountComponent/>
             </div>
         );
     }
